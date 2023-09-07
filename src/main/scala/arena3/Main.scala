03b6c4bf1dd280a31171818b45b0d946b565afbe
Yaxin <137126846@qq.com>
2023-09-07 20:50:09 +0800

    [arena3] add init question.

--- src/main/scala/arena3/Main.scala
+++ src/main/scala/arena3/Main.scala
@@ -1,4 +1,20 @@
 package arena3
 
 object Main extends App:
-  println("hello, scala3")
+  val program = for
+    _ <- write("Input:")
+    input <- read
+    _ <- write("Output:")
+    _ <- write(input)
+  yield input
+  // 此行以上没有任何Effect
+
+  program.run
+  // Console输出: Input:
+  // Console等待输入
+  // Console输出: Output:
+  // Console输出: 上一行输入结果
+
+  println("=============")
+  program.loopWhile(_.nonEmpty).run
+// 进行上述行为的循环直到输入空值
