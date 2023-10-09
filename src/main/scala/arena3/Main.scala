e883fa8e2f3ec0a6c552d381800a0dea29bd86d9
Yaxin <137126846@qq.com>
2023-10-09 22:58:04 +0800

    [arena3] Seperate effect and implementation.

--- src/main/scala/arena3/Main.scala
+++ src/main/scala/arena3/Main.scala
@@ -1,5 +1,6 @@
 package arena3
 
+import scala.io.StdIn.readLine
 object Main extends App:
   val program = for
     _ <- write("Input:")
@@ -18,3 +19,26 @@ object Main extends App:
   println("=============")
   program.loopWhile(_.nonEmpty).run
 // 进行上述行为的循环直到输入空值
+
+sealed trait ConsoleEffect[A]:
+  def flatMap[B](f: A => ConsoleEffect[B]): ConsoleEffect[B] = ChainEffect(this, f)
+  def map[B](f: A => B): ConsoleEffect[B] = this.flatMap(a => NoEffect(f(a)))
+  def loopWhile(p: A => Boolean): ConsoleEffect[A] = this.flatMap(a => if (p(a)) loopWhile(p) else NoEffect(a))
+
+  final def run: A = this match
+    case Read        => readLine()
+    case Write(v)    => println(v)
+    case NoEffect(a) => a
+    case ChainEffect(fa, f) =>
+      fa match
+        case Read                 => f(readLine()).run
+        case Write(v)             => f(println(v)).run
+        case NoEffect(a)          => f(a).run
+        case ChainEffect(fa1, f1) => fa1.flatMap(a => f1(a).flatMap(f)).run
+case object Read extends ConsoleEffect[String]
+case class Write(v: String) extends ConsoleEffect[Unit]
+case class NoEffect[A](a: A) extends ConsoleEffect[A]
+case class ChainEffect[A, B](fa: ConsoleEffect[A], f: A => ConsoleEffect[B]) extends ConsoleEffect[B]
+
+def read = Read
+def write(v: String) = Write(v)
