094167535dff843d5ac78b4a111c9293b02e949b
Yaxin <137126846@qq.com>
2023-11-02 17:08:39 +0800

    [Type Class]Add extension to express add method to existing type A.

--- src/main/scala/MyShow.scala
+++ src/main/scala/MyShow.scala
@@ -1,11 +1,13 @@
-
 trait MyShow[A]:
   def myShow(a: A): String
+  extension (a: A)
+    def anotherShow: String = myShow(a)
 
 object MyShow:
   given MyShow[String] with
     override def myShow(a: String): String = "show: "+ a
 
 object Main extends App:
-  val result = summon[MyShow[String]].myShow("hh")
+  import MyShow.given
+  val result = "hh".anotherShow
   println(result)
